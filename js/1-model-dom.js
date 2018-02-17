c1e92da77921d1ff2f8f3050eb730648f40265ae
Kamil Predel <kamil.predel@gmail.com>
2018-02-17 11:30:30 +0100

    model-dom

---
+++ js/1-model-dom.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log (window);
+console.log (window.innerHeight);
+console.log (window.outerHeight);
+
+console.log (document);
+console.log (document.documentElement);
+console.log (document.document.body);
+console.log (document.document.head);
